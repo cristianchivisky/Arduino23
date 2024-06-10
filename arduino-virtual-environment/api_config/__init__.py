d725db988e3ebf62bb5fd20d938d8a6f83174544
Cristian Chivisky <cristian.chivisky@gmail.com>
2024-06-10 12:58:35 -0300

    agregando optimizaciones

--- arduino-virtual-environment/api_config/__init__.py
+++ arduino-virtual-environment/api_config/__init__.py
@@ -8,6 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask('MyApp')
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:lacava646@localhost/Infraciones"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://postgres:password@localhost/name_of_database"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
