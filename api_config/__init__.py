dbb301d29d64a037270805afbdcdc6e93eb74bf8
Cristian Chivisky <cristian.chivisky@gmail.com>
2024-06-11 18:09:16 -0300

    otras modificaciones mas

--- api_config/__init__.py
+++ api_config/__init__.py
@@ -8,6 +8,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask('MyApp')
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:lacava646@localhost/infracciones"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:nombre_usuario:tu_contraseña@localhost/nombre_base_de_datos"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
