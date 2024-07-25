f92e197a57a18a06659446809b8a898360c169fe
Cristian Chivisky <cristian.chivisky@gmail.com>
2024-07-25 16:51:38 -0300

    cambios para desplegar

--- api_config/__init__.py
+++ api_config/__init__.py
@@ -3,11 +3,11 @@ from flask import (
 )
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask('MyApp')
 CORS(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://nombre_usuario:tu_contraseña@localhost/nombre_base_de_datos"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get('DATABASE_URL') 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
